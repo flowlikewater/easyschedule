ee65c39b00d14c4f57b373a81610f169d8b8d4a4
kenNg1 <kng1230@hotmail.com>
2017-05-19 18:53:58 +0800

    add update and delete func

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,10 +17,26 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: {errors: user.errors}, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy()
+    # we are giving 204 code which means the server has processed the request and doesn't return any content
+    head 204
+  end
+
   # The private method user_params sanitizes the attributes to be assigned through mass-assignment
   private
   def user_params
     params.permit(:email,:password,:password_confirmation)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json  } do
     devise_for :users
     namespace :v1 do
-      resources :users, :only => [ :show, :create ]
+      resources :users, :only => [ :show, :create, :update, :destroy ]
     end
   end
 
