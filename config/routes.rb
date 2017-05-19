2047c1a41538ce1ada9d4eae0854142a7c02be66
kenNg1 <kng1230@hotmail.com>
2017-05-19 17:28:10 +0800

    added resources users line, moved user controller to api folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  root :to => 'main#index'
+
   devise_for :users
+
   namespace :api, defaults: {format: :json  } do
     namespace :v1 do
-       
+      resources :users, :only => [ :show, :create ]
     end
   end
-
-  root :to => 'main#index'
 
 end
