c72b6bb15985484d6f253105d285aabcba5cd6d0
kenNg1 <kng1230@hotmail.com>
2017-05-19 17:17:32 +0800

    namespace api, create api version folders, devise, devise g User, create and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  namespace :api, defaults: {format: :json  } do
+    namespace :v1 do
+       
+    end
+  end
+
   root :to => 'main#index'
 
 end
