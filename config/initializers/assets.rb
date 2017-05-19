76e4df0bdf4e1b6322a40fe83101fc559824f98f
kenNg1 <kng1230@hotmail.com>
2017-05-19 16:53:52 +0800

    add active_model_serialisers, commit before heroku deploy

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
+Rails.application.config.assets.precompile += %w( yeti-bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( fullcalendar.min.js )
 Rails.application.config.assets.precompile += %w( fullcalendar.css )
-
-Rails.application.config.assets.precompile += %w( fullcalendar.js )
+Rails.application.config.assets.precompile += %w( moment.min.js )
