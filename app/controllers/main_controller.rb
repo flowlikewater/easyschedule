f50133f8b883ab4615566c642bed15e62eb4e56a
kenNg1 <kng1230@hotmail.com>
2017-05-21 07:00:18 +0800

    ajax request for user info

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,15 @@ class MainController < ApplicationController
                       )
     # object class is important here to help turn it into a user friendly format
     @room = JSON.parse(response.body, object_class:OpenStruct)
+
+    response2 = RestClient::Request.execute(
+                      method: :get,
+                      url: "localhost:3000/api/v1/users/#{params[:token]}.json",
+                      headers: {api_key: Rails.application.secrets.BOOKING_API_KEY, token: params[:token]}
+                      )
+    # object class is important here to help turn it into a user friendly format
+    @user = JSON.parse(response2.body, object_class:OpenStruct)
+
   end
 
 end
