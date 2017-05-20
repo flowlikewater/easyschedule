ec439f28f2d7b2c757906e1098e9e3ae6cb24179
kenNg1 <kng1230@hotmail.com>
2017-05-21 05:30:01 +0800

    proof that two params can be passed via api

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,13 @@
 class MainController < ApplicationController
 
   def index
-    @users = User.all()
+    response = RestClient::Request.execute(
+                      method: :get,
+                      url: "localhost:3000/api/v1/rooms/#{params[:room]}.json",
+                      headers: {api_key: Rails.application.secrets.BOOKING_API_KEY, token: params[:token]}
+                      )
+    # object class is important here to help turn it into a user friendly format
+    @room = JSON.parse(response.body, object_class:OpenStruct)
   end
 
 end
